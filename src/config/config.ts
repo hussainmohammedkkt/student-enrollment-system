ea29c1922ef3977411a8e9b76398d59e4120ce24
Mohammed Hussain <mohammed.hussain@wiot360.com>
2025-01-22 21:16:39 +0400

    Add port from .env

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,4 +3,5 @@ require('dotenv').config();
 export const config = {
   jwt_secret: process.env.JWT_SECRET ?? 'your_secret_key',
   mongo_uri: process.env.MONGO_URI ?? '',
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
 };
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,19 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AdminService } from './user/admin.service';
+import { config } from './config/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Student Enrollment API')
     .setDescription('API for managing students, courses, and enrollments')
     .setVersion('1.0')
     .addTag('students')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
 
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(
@@ -27,6 +28,6 @@ async function bootstrap() {
   const adminService = app.get(AdminService);
   adminService.createSuperAdmin();
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(config.port);
 }
 bootstrap();
